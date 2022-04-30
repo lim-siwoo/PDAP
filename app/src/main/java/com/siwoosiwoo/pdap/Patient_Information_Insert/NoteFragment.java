a91f9cbf2979725e949ca11964910ec3d5fb2590
SnowArtics <84330600+SnowArtics@users.noreply.github.com>
2022-05-01 01:24:05 +0900

    Update database and update Date fix

---
+++ app/src/main/java/com/siwoosiwoo/pdap/Patient_Information_Insert/NoteFragment.java
@@ -0,0 +1,21 @@
+package com.siwoosiwoo.pdap.Patient_Information_Insert;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.siwoosiwoo.pdap.R;
+
+public class NoteFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_note, container, false);
+    }
+}
