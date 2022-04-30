b989d38ecf1603c266d5ae05a16f74f38ffa4eed
임시우 (Siwoo Lim) <siwoo5530@gmail.com>
2022-04-30 22:27:49 +0900

    Add addNewPatientActivity

---
+++ app/src/main/java/com/siwoosiwoo/pdap/addNewPatientActivity.java
@@ -0,0 +1,14 @@
+package com.siwoosiwoo.pdap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class addNewPatientActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_patient);
+    }
+}
